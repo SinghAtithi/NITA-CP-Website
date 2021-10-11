65ee784ec69e013c162a2f241a94af9a483efa4a
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2021-10-12 00:13:50 +0530

    fixing some bugs

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Image from "next/image"
 
 function BlogCard() {
   return (
     <div className="m-6">
       <div className="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
         <a href="#" className="w-full block h-full">
-          <img
-            alt="blog photo"
+        <Image
             src="/images/person6.webp"
+            alt="Picture of the blog"
+            width={330}
+            height={190}
             className="max-h-40 w-full object-cover"
           />
+          
           <div className="bg-white dark:bg-gray-800 w-full p-4">
             <p className="text-indigo-500 text-md font-medium">Article</p>
             <p className="text-gray-800 dark:text-white text-xl font-medium mb-2">
