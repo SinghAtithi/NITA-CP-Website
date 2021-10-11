1fe7768f3d3eb996259e5e35b56e9ecd42a4666c
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2021-10-12 00:29:26 +0530

    hurrey

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
 import Resourcecard from "../components/Resourcecard";
 import Tryy from "../components/Tryy";
+import Homee from "../components/Homee";
 
 export default function Home() {
   return (
     <div className="home">
       {/* Navbar */}
       <Navbar />
+      <Homee/>
     </div>
   );
 }
